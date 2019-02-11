2c3b327a15b695dcdb84ed72bcd0046dfdca8cd0
Adrien Richard <richardadrien0@gmail.com>
2019-02-11 13:25:30 +0100

    Updates Files

--- google.sh
+++ google.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sudo links2 http://www.google.com/
+#!/bin/bash
+sudo links2 http://www.google.com/
--- showDisks.sh
+++ showDisks.sh
@@ -2,3 +2,12 @@
 sudo lsblk;
 echo -e "\n---\n";
 sudo ls -l /dev/disk/by-uuid/;
+#!/bin/bash
+clear;
+echo -e "---lsblk---\n";
+sudo lsblk;
+echo -e "\n---Disk UUID---\n";
+sudo ls -l /dev/disk/by-uuid/;
+echo -e "\n---df---\n";
+df -h;
+echo -e "\n";
